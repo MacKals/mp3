a5b05571e90504eacc6519751bce22d2006fa468
Erik MacLennan <erikmaclennan@me.com>
2015-10-23 11:20:14 -0700

    small changes

--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -1,7 +1,9 @@
 package ca.ubc.ece.cpen221.mp3.graph;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
