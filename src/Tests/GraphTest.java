295ed9251936652d4b59a6517ea65d1bd1c4c3f7
mKals <m.kals@me.com>
2015-10-23 22:15:34 -0700

    stuff

--- src/Tests/GraphTest.java
+++ src/Tests/GraphTest.java
@@ -2,18 +2,44 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.List;
+
 import org.junit.Test;
 
 import TwitterAnalysis.TwitterAnalysis;
+import ca.ubc.ece.cpen221.mp3.graph.Algorithms;
+import ca.ubc.ece.cpen221.mp3.staff.Graph;
+import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class GraphTest {
 
+    private static String twitterFile = "datasets/twitter.txt";
+    private static String simpleFile = "datasets/SimpleDataset.txt";
+    private static String mediumFile = "datasets/MediumDataset.txt";
+    
     @Test
     public void test() {
         
-        TwitterAnalysis main = new TwitterAnalysis();
+        File file = new File(mediumFile);
+        Graph graph = TwitterAnalysis.fileToGraph(file);
         
+        System.out.println("File loaded");
         
+        Vertex a = new Vertex("1"); //fill in string
+        Vertex b = new Vertex("5"); //fill in string
+        
+        int retweets = Algorithms.shortestDistance(graph, a, b);
+        
+        assertEquals(retweets, 1);
+        
+        List<Vertex> commonInfluencers = Algorithms.commonDownstreamVertices(graph, a, b);
+        
+        assertEquals(vertex.toString(), );
+
+        
+        for (Vertex vertex : commonInfluencers) {
+        }
         
     }
 
--- src/TwitterAnalysis/TwitterAnalysis.java
+++ src/TwitterAnalysis/TwitterAnalysis.java
@@ -2,24 +2,19 @@ package TwitterAnalysis;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
 import ca.ubc.ece.cpen221.mp3.graph.AdjacencyListGraph;
-import ca.ubc.ece.cpen221.mp3.graph.AdjacencyMatrixGraph;
 import ca.ubc.ece.cpen221.mp3.graph.Algorithms;
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 
-/** Performs an analys
- * 
+/** Performs an analysis on a dataset from Twitter.
  * @author erikmaclennan, mkals
- *
  */
 public class TwitterAnalysis {
 
@@ -59,11 +54,22 @@ public class TwitterAnalysis {
     
     private static List<String> getQueriesFromFile(File file) {
         
+        String commonInfluencersTag = "commonInfluencers"; 
+        String "numRetweets"
+        
+        //<query type> <user a> <user b>
+        
         
         
     }
     
-    private static String printCommonInfluencers(List<Vertex> result, Vertex v1, Vertex v2) {
+    /** Prints to the console each of the users that are followed by two specified users.
+     *  Prints each user identifier on a new line, surrounded by html "result" tags
+     * @param result a list of all common influencers. Each user is represented by a vertex object
+     * @param v1 a vertex corresponding to a Twitter user.
+     * @param v2 another vertex corresponding to a Twitter user.
+     */
+    private static void printCommonInfluencers(List<Vertex> result, Vertex v1, Vertex v2) {
         
         System.out.println("query: commonInfluencers " + v1.toString() + " " + v2.toString()); 
         System.out.println(openingToken);
@@ -74,10 +80,15 @@ public class TwitterAnalysis {
 
         System.out.println(closingToken);
         
-        return null;
     }
     
-    private static String printNumRetweets(int result, Vertex v1, Vertex v2) {
+    /** Prints to the console the minimum number of retweets needed before v1's tweet appears in v2's feed.
+     * This integer is surrounded by html "result" tags.
+     * @param result an integer representing the minimum number of retweets
+     * @param v1 first user.    
+     * @param v2 second user.
+     */
+    private static void printnumRetweets(int result, Vertex v1, Vertex v2) {
         
         System.out.println("query: numRetweets " + v1.toString() + " " + v2.toString()); 
         System.out.println(openingToken);
@@ -86,7 +97,12 @@ public class TwitterAnalysis {
         
     }
     
-    public static Graph fileToGraph(File file){
+    /** Reads data from a file and returns a Graph object representation that represents a Directed Graph.
+     * The file must be contain two user identifiers on each line, separated by an "->".
+     * @param file the file to read from.
+     * @return A graph that represents the data from the file as a Directed Graph.
+     */
+    private static Graph fileToGraph(File file){
         
         Graph buildGraph = new AdjacencyListGraph();
         
