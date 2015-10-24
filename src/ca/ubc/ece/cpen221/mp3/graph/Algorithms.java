16398218c774dc89e7b18f396da6e0e6c9600b6c
Erik MacLennan <erikmaclennan@me.com>
2015-10-23 20:47:16 -0700

    specs for all algorithms

--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -14,42 +14,35 @@ import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class Algorithms {
-
-	/**
-	 * *********************** Algorithms ****************************
-	 * 
-	 * Please see the README for the machine problem for a more detailed
-	 * specification of the behavior of each method that one should implement.
-	 */
-
-	/**
-	 * This is provided as an example to indicate that this method and other
-	 * methods should be implemented here.
-	 * 
-	 * You should write the specs for this and all other methods.
-	 * 
-	 * @param graph
-	 * @param a
-	 * @param b
-	 * @return
-	 */
 	
-	/**
-	 * 
-	 */
+    /** Performs a "Breadth First Search" on a directed graph of vertices.
+     * @param graph the directed graph to search through.
+     * @return a set of lists, with each list corresponding to the vertices connected via edge to a vertex. 
+     * The order of this list corresponds to the order in which the vertices were traversed in the search.
+     */
 	public static Set<List<Vertex>> BFS(Graph graph) {
 	   
 	    return search(graph, true);
 	}
 	
-	/**
-	 * 
+	/** Performs a "Depth First Search" on a directed graph of vertices.
+	 * @param graph the directed graph to search through.
+	 * @return a set of lists, with each list corresponding to the vertices connected via edge to a vertex. 
+	 * The order of this list corresponds to the order in which the vertices were traversed in the search.
 	 */
 	public static Set<List<Vertex>> DFS(Graph graph) {
 	    	    
 	    return search(graph, false);
 	}
 	
+	/** Searches through a Directed Graph to find a set of lists with each list corresponding to a vertex.
+	 * The number of lists corresponds to the number of vertices in the graph. Each list holds the vertices 
+	 * connected via edge to the given vertex, in the order they were searched.
+	 * @param graph The Directed Graph to be searched. 
+	 * @param BFS true if "Breadth First Search" is desired, false if "Depth First Search" is desired.
+	 * @return a set of lists, with each list corresponding to the vertices connected via edge to a vertex. 
+	 * Each list represents a vertex in the graph, with the order of these list corresponding to the desired search method.
+	 */
 	private static Set<List<Vertex>> search(Graph graph, boolean BFS) {
 	    
 	    Set<List<Vertex>> returnSet = new TreeSet<List<Vertex>>();
@@ -63,7 +56,14 @@ public class Algorithms {
         return returnSet;
     
 	}
-	
+	/** Creates an ordered list of all the vertices connected to a vertex from a graph, 
+	 * with this order determined by either BFS or DFS methods 
+	 * @param graph The Directed Graph that contains the vertex. 
+	 * @param vertex The vertex from which to start the search from. must be within "graph".
+	 * @param BFS true if a "Breadth First Search" is desired, false if a "Depth First Search" is desired.
+	 * @return a list of all the vertices connected via edge to "vertex" in the order they were searched.
+	 * This order is determined by which search method is desired.
+	 */
 	private static List<Vertex> listFromVertex(Graph graph, Vertex vertex, boolean BFS) {
 	    
 	    List<Vertex> returnList = new ArrayList<Vertex>();
@@ -107,12 +107,12 @@ public class Algorithms {
 	    return returnList;
 	}
 	
-	/**
-	 * Get all upstream vertices with a direct edge to both vertex a and vertex b
-	 * @param graph Graph to be analysed 
-	 * @param a Vertex
-	 * @param b Vertex 
-	 * @param list of common downstream neighbours of a and b
+	/** Computes all downstream vertices with a direct edge to both vertex a and vertex b 
+	 * @param graph Graph to be analyzed 
+	 * @param a first vertex
+	 * @param b second vertex 
+	 * @return list of common upstream neighbours of a and b.
+	 * If there are no upstream neighbours, returns an empty list. 
 	 */
 	public static List<Vertex> commonUpstreamVertices(Graph graph, Vertex a, Vertex b) {
 	    
@@ -138,12 +138,12 @@ public class Algorithms {
 	    return Collections.unmodifiableList(commonUpstreamVertices);
 	}
 	
-    /**
-     * Get all downstream vertices with a direct edge to both vertex a and vertex b
-     * @param graph Graph to be analysed 
-     * @param a Vertex
-     * @param b Vertex 
-     * @param list of common upstream neighbours of a and b
+    /** Computes all downstream vertices with a direct edge to both vertex a and vertex b
+     * @param graph Graph to be analyzed 
+     * @param vertA first vertex
+     * @param vertB second vertex
+     * @return list of common downstream neighbours to both vertA and vertB. 
+     * If there are no downstream neighbours, returns an empty list.
      */
 	public static List<Vertex> commonDownstreamVertices(Graph graph, Vertex vertA, Vertex vertB){
 	    
@@ -165,6 +165,14 @@ public class Algorithms {
 	    int evaluate(Vertex v, boolean upstream);   
 	}
 	
+	/** Computes the shortest distance between two vertices in a Directed Graph.
+	 * The distance is equal to the shortest number of edges between the two vertices.
+	 * @param graph the graph to search through.
+	 * @param a the first vertex.
+	 * @param b the second vertex.
+	 * @return an integer of the smallest number of edges between vertex a and b. 
+	 * Returns -1 if there is no path between the two vertices.
+	 */
     public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
                         
         int NotFound = -1;
