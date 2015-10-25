f9b549a3453c75782491bb7aef7dd35d772f27eb
Erik MacLennan <erikmaclennan@me.com>
2015-10-24 19:05:17 -0700

    small changes

--- src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/AdjacencyListGraph.java
@@ -22,10 +22,10 @@ public class AdjacencyListGraph implements Graph {
            
            //we found the vertex v1, and we don't have this edge already, then add it
             if (adjacencyList.get(i).get(0).equals(v1) && !adjacencyList.get(i).contains(v2)){ 
+                
                 adjacencyList.get(i).add(v2); //add the edge
                 break;
             }
-           
         }
         
     }
